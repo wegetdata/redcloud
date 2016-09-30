d78811e7af20d863e2d3e61b336490dbffb09e33
root <technomad@protonmail.com>
2016-09-30 20:32:54 +0000

    new update

--- project/models.py
+++ project/models.py
@@ -8,14 +8,14 @@ db = SQLAlchemy(app)
 
 
 class Project(db.Model):
-    name = db.Column(db.String, primary_key=True)
-    category = db.Column(db.String, index=True, unique=True, nullable=False)
-    data = db.Column(db.String, index=True, unique=True, nullable=False)
+    secretkey = db.Column(db.String(900), primary_key=True)
+    category = db.Column(db.String(900), index=True, unique=True, nullable=False)
+    folder = db.Column(db.String, index=True, unique=True, nullable=False)
 
-    def __init__(self, name, category, data):
-        self.name = name
+    def __init__(self, secretkey, category, folder):
+        self.secretkey = secretkey
         self.category = category
-        self.data = data
+        self.folder = folder
 
     def __repr__(self):
-        return '<Project Name %r>' % self.name
+        return '<Project Name %r>' % self.secretkey
--- project/views.py
+++ project/views.py
@@ -3,12 +3,14 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-
+from flask import Markup, flash
 from project.models import *
 from project.controllers import hello
 from flask import redirect, url_for
-app = Flask(__name__)
+import os.path
 
+app = Flask(__name__)
+app.secret_key = '1l2jk3lk12j3lk21j3lk21j3l2j3l12j3l12j3l2j3l213'
 
 
 
@@ -19,21 +21,27 @@ def index():
 	return render_template('starter-template.html')
     
 
-
-
+                           
 
 @app.route('/search', methods=['POST'])
 def search():
-    name=request.form['name']
+    secretkey=request.form['name']
+    return redirect(url_for('project', secretkey=secretkey))
     
-    return redirect(url_for('project', name=name))
     
-@app.route('/project/<name>')
-def project(name):
-    project = Project.query.filter_by(name=name).first()
+    
+@app.route('/project/<secretkey>')
+def project(secretkey):
+    project = Project.query.filter_by(secretkey=secretkey).first()
+    
+    #paths
+    
+    if os.path.exists('/root/redcloud/files/'+ project.folder +'/port/index.txt'):
+        content = open('/root/redcloud/files/'+ project.folder +'/port/index.txt').read()
+    else:
+        content = ""
     return render_template('fluid.html',
-                           project=project)
-                           
+                            project=project, content=content)
                            
 # Error Pages
 @app.errorhandler(500)
@@ -44,9 +52,6 @@ def error_page(e):
 def not_found(e):
     return render_template('error_pages/404.html'), 404
 
-
-# Lazy Views
-app.add_url_rule('/hello', view_func=hello.hello_world)
 
 
 
